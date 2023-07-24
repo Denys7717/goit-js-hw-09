f1f937318b0d6f63fcbe8a1f9ae0d0519428c463
Denys7717 <127441595+Denys7717@users.noreply.github.com>
2023-07-24 18:02:59 +0000

    Deploying to gh-pages from @ Denys7717/goit-js-hw-09@abe955121cf6d72323ac0c0f3b0d78dd4a82de04 🚀

---
+++ 01-color-switcher.c21fdb2d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),n=null;t.addEventListener("click",onClick((function(){t.setAttribute("disabled",""),e.removeAttribute("disabled"),n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}))),e.addEventListener("click",stopChange((function(){t.removeAttribute("disabled"),e.setAttribute("disabled",""),clearInterval(n)})))}();
+//# sourceMappingURL=01-color-switcher.c21fdb2d.js.map

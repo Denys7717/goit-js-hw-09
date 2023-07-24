5f121c50f46d49a4629c3eb5f700209b0d69e347
Denys7717 <127441595+Denys7717@users.noreply.github.com>
2023-07-24 17:50:03 +0000

    Deploying to gh-pages from @ Denys7717/goit-js-hw-09@d388fd958acd40bc208a726bc10b6ae39023bfa3 🚀

--- 01-color-switcher.60b70f9d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),a=null;start.addEventListener("click",onClick=function(){t.setAttribute("disabled",""),e.removeAttribute("disabled"),a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}),e.addEventListener("click",stopChange=function(){t.removeAttribute("disabled"),e.setAttribute("disabled",""),clearInterval(a)})}();
-//# sourceMappingURL=01-color-switcher.60b70f9d.js.map
